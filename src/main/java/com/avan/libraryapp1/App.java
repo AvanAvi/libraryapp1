278c783e63a046a8aab5b98e4d88ad5851da5425
Apple <apple@Apples-MacBook-Air.local>
2023-10-18 20:37:29 +0200

    fix: Code smell fixes

--- src/main/java/com/avan/libraryapp1/App.java
+++ src/main/java/com/avan/libraryapp1/App.java
@@ -1,17 +1,23 @@
 package com.avan.libraryapp1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+  // Define a static logger variable so that it references the Logger instance
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
 
     SpringApplication app = new SpringApplication(App.class);
     app.run(args);
     
-    System.out.println("Library App Started!");
+    // Use the logger to log the message instead of System.out.println
+    logger.info("Library App Started!");
 
   }
 
