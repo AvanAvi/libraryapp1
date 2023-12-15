13ae85438fcdf75c48d1bf53dd99d5dd2da0a449
Apple <apple@Apples-MacBook-Air.local>
2023-12-15 05:29:10 +0100

    Refactor BookServiceTests to mock BookRepository.findById()

--- src/test/java/com/avan/libraryapp1/unittests/BookServiceTests.java
+++ src/test/java/com/avan/libraryapp1/unittests/BookServiceTests.java
@@ -1,36 +1,48 @@
 package com.avan.libraryapp1.unittests;
+
 import java.util.List;
 import java.util.Collections;
-import org.mockito.Mockito;
 import java.util.Arrays;
-
+import java.util.Optional;
 
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.Optional;
 
 import com.avan.libraryapp1.model.Book;
 import com.avan.libraryapp1.repository.BookRepository;
 import com.avan.libraryapp1.services.BookService;
 
 @ExtendWith(MockitoExtension.class)
-public class BookServiceTests {
 
-    @InjectMocks
-    private BookService bookService;
+public
+ 
+class
+ 
+BookServiceTests
+ 
+{
 
     @Mock
-    private BookRepository bookRepository;
+
+    
+private BookRepository bookRepository;
+
+    @InjectMocks
+
+    
+private BookService bookService;
 
     @Test
     public void testGetAllBooks_EmptyList() {
         // Mock the behavior of BookRepository.findAll() to return an empty list
-        Mockito.when(bookRepository.findAll()).thenReturn(Collections.emptyList());
+        when(bookRepository.findAll()).thenReturn(Collections.emptyList());
 
         // Call the getAllBooks() method on the BookService
         List<Book> books = bookService.getAllBooks();
@@ -40,46 +52,32 @@ public class BookServiceTests {
     }
 
     @Test
-    public void testGetBookById_InvalidID() {
-        // Mock the behavior of BookRepository.findById() to return an empty Optional
-        Mockito.when(bookRepository.findById(1L)).thenReturn(Optional.empty());
-
-        // Call the getBookById() method on the BookService with an invalid ID
-        Optional<Book> book = bookService.getBookById(1L);
-
-        // Verify that the assertion fails because the book is not found
-        Assertions.assertTrue(book.isEmpty());
-    }
-
-    @Test
     public void testGetBookById_ValidID() {
-        // Create a Book object with an ID of 1
-        Book book = new Book();
-        book.setId(1L);
-        book.setTitle("Book 1");
-        book.setAuthor("Author 1");
+        // Create an Optional<Book> object
+        Optional<Book> book = Optional.of(new Book());
 
-        // Mock the behavior of BookRepository.findAll() to return a list containing the book
-        List<Book> books = Arrays.asList(book);
-        Mockito.when(bookRepository.findAll()).thenReturn(books);
+        // Mock the behavior of BookRepository.findById() to return the Optional<Book>
+        when(bookRepository.findById(1L)).thenReturn(book);
 
         // Call the getBookById() method on the BookService with the valid ID
         Optional<Book> retrievedBook = bookService.getBookById(1L);
 
-        // Verify that the assertion passes because the book is found and the Optional is not empty
+        // Verify that the assertion succeeds because the book is found and the Optional is not empty
         Assertions.assertTrue(retrievedBook.isPresent());
-        Assertions.assertEquals(book, retrievedBook.get());
+        Assertions.assertEquals(book.get(), retrievedBook.get());
     }
+
+
 
     @Test
     public void testGetBookById_NotFoundBook() {
-        // Mock the behavior of BookRepository.findById() to throw an RuntimeException
-        Mockito.when(bookRepository.findById(1L)).thenThrow(new RuntimeException());
+        // Mock the behavior of BookRepository.findById() to return an empty Optional
+        when(bookRepository.findById(1L)).thenReturn(Optional.empty());
 
         // Call the getBookById() method on the BookService with the ID
         Optional<Book> book = bookService.getBookById(1L);
 
-        // Verify that the assertion fails because an exception is thrown
-        Assertions.assertTrue(book.isEmpty());
+        // Verify that the assertion succeeds because the book is not found and the Optional is empty
+        Assertions.assertFalse(book.isPresent());
     }
 }
